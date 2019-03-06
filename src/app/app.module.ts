e9c4dd0440e9e8a7a8180eec329e67998f73f51b
hphucc <hphucc1907@gmail.com>
2019-03-06 09:47:13 +0700

    ss

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { AppmenuComponent } from './components/appmenu/appmenu.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppsettingsComponent } from './components/appsettings/appsettings.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    AppheaderComponent,
+    AppmenuComponent,
+    AppfooterComponent,
+    AppsettingsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
